40bb73db20ef1970c58245382937cf0eba6c73ea
Andrei Maiboroda <andrei@ethereum.org>
2019-07-25 16:27:40 +0200

    Unit tests for SELFBALANCE opcode

--- test/unittests/libevm/VMTest.cpp
+++ test/unittests/libevm/VMTest.cpp
@@ -913,7 +913,6 @@ BOOST_AUTO_TEST_CASE(LegacyVMSelfBalanceisInvalidBeforeIstanbul)
     testSelfBalanceisInvalidBeforeIstanbul();
 }
 BOOST_AUTO_TEST_SUITE_END()
-
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_FIXTURE_TEST_SUITE(AlethInterpreterSuite, TestOutputHelperFixture)
